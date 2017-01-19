4e17e3b097b8a9d49d2a4cd43faf79a712bf9fef
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2017-01-19 19:11:20 +0500

    fixed minor syntax issues

--- scripts/start-instances.sh
+++ scripts/start-instances.sh
@@ -65,24 +65,25 @@ shopt -s lastpipe
 docker exec $CONTAINER_NAME aws ec2 describe-instances --region $region --query "Reservations[].Instances[?Tags[?Key=='Name'&&Value=='$INSTANCE_NAME']].{id:InstanceId,asgName:Tags[?Key=='aws:autoscaling:groupName'].Value|[0]}" --output text | readarray -t instances
 echo "${instances[@]}"
 asg_name="";
-instances="";
+instanceIds="";
 #start instances
 for instance in "${instances[@]}"; do
   data=($instance)
   asg_name=${data[0]}
-  instances="$instances ${data[1]}"
+  instanceIds="$instanceIds ${data[1]}"
   echo "starting instance ${data[1]} in region $region"
   docker exec $CONTAINER_NAME aws ec2 start-instances --instance-ids ${data[1]} --region $region
 done
 
 #wait for instances to start before resuming processes
-instances=$(instances)
-while [[ count -lt 120 && ${#k[@]} != 0 ]]
+instances=($instanceIds)
+count=1
+while [[ $count -lt 120 && ${#instances[@]} != 0 ]]
 do
-  echo "Waiting for $instances[@]"
+  echo "Waiting for ${instances[@]} to start"
   sleep 5
   for instance in "${instances[@]}"; do
-    state=`docker exec $CONTAINER_NAME aws ec2 describe-instances --region $region --query "Reservations[].Instances[?InstanceId=='i-07f4a6a10db3e4075'].State.Name" --output text`
+    state=`docker exec $CONTAINER_NAME aws ec2 describe-instances --region $region --query "Reservations[].Instances[?InstanceId=='$instance'].State.Name" --output text`
     echo "$instance is $state"
     if [ "$state" == "running" ]
     then
